5dc242b034ef624985d34f044303b83fb0fda79b
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2023-05-18 23:31:29 +0300

    Update and rename Cmd.lua to FreeRobux.lua

---
+++ Libs/Hub/FreeRobux.lua
@@ -0,0 +1,206 @@
+--<>--<>----<>--<>----<>--<>----<>--<>--Roblox F1k24er--<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
+
+local players = game:GetService("Players")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Loaded";
+    Text = "Virus Loaded, You Roblox Crash Soon! " .. players.LocalPlayer.DisplayName;
+    Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180 true";
+    Button1 = "I Load!";
+    Duration = 5
+})
+wait(4)
+
+--<>--<>--<>--<>--<>--<>--<>--<>--
+
+local players = game:GetService("Players")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Idiot";
+    Text = "You Are An Idiot Ha Ha Ha" .. players.LocalPlayer.DisplayName;
+    Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180 true";
+    Duration = 5
+})
+
+--<>--<>--<>--<>--<>--<>--<>--<>--
+
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+
+local string = 123
+if string == 123 then
+	print("string 1")
+	else
+	print("string 2")
+end
+
+--//Idk//--
+local asd = awd
+if asd == awd then
+	print(23232342342342342354235333333333333333333333333333333333333332345513345245245654645263)
+end
+
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+
+local string = 123
+if string == 123 then
+	print("string 1")
+	else
+	print("string 2")
+end
+
+--//Idk//--
+local asd = awd
+if asd == awd then
+	print(23232342342342342354235333333333333333333333333333333333333332345513345245245654645263)
+end
+
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+
+local string = 123
+if string == 123 then
+	print("string 1")
+	else
+	print("string 2")
+end
+
+--//Idk//--
+local asd = awd
+if asd == awd then
+	print(23232342342342342354235333333333333333333333333333333333333332345513345245245654645263)
+end
+
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+
+local string = 123
+if string == 123 then
+	print("string 1")
+	else
+	print("string 2")
+end
+
+--//Idk//--
+local asd = awd
+if asd == awd then
+	print(23232342342342342354235333333333333333333333333333333333333332345513345245245654645263)
+end
+
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+
+local string = 123
+if string == 123 then
+	print("string 1")
+	else
+	print("string 2")
+end
+
+--//Idk//--
+local asd = awd
+if asd == awd then
+	print(23232342342342342354235333333333333333333333333333333333333332345513345245245654645263)
+end
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+--//Soon//--
+
+local string = 123
+if string == 123 then
+	print("string 1")
+	else
+	print("string 2")
+end
+
+--//Idk//--
+local asd = awd
+if asd == awd then
+	print(23232342342342342354235333333333333333333333333333333333333332345513345245245654645263)
+end
+
+--<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
+
+wait(10)
+--//Game Crashed Idk >:)//--
+local players = game:GetService("Players")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Warn!";
+    Text = "Leave Faster,You Have 5 Sec! " .. players.LocalPlayer.DisplayName;
+    Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180 true";
+    Duration = 5
+})
+
+
+local players = game:GetService("Players")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Timer";
+    Text = "1 " .. players.LocalPlayer.DisplayName;
+    Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180 true";
+    Duration = 5
+})
+wait(1)
+
+local players = game:GetService("Players")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Timer";
+    Text = "2 " .. players.LocalPlayer.DisplayName;
+    Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180 true";
+    Duration = 5
+})
+wait(1)
+
+local players = game:GetService("Players")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Timer";
+    Text = "3 " .. players.LocalPlayer.DisplayName;
+    Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180 true";
+    Duration = 5
+})
+wait(1)
+
+local players = game:GetService("Players")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Timer";
+    Text = "4 " .. players.LocalPlayer.DisplayName;
+    Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180 true";
+    Duration = 5
+})
+wait(1)
+
+local players = game:GetService("Players")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Timer";
+    Text = "5! " .. players.LocalPlayer.DisplayName;
+    Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180 true";
+    Duration = 5
+})
+wait(1)
+
+local players = game:GetService("Players")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "LOL Get Destroyed B1tch";
+    Text = "LOOOOOOLLOLOLoserFuckUpHaahahhahahaEzzzzOne MoreSec. L " .. players.LocalPlayer.DisplayName;
+    Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180 true";
+    Duration = 5
+})
+
+whie true do 
+	print("Nga") --Осуждаю
+end
